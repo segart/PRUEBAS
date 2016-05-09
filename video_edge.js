1ae5b24f93e11d59253dab2e78e6bc63753c43fd
segart <sergio@segart.com>
2016-05-09 14:18:00 -0500

    Add files via upload

---
+++ video_edge.js
@@ -0,0 +1,9 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x6='rgba(0,0,0,0)',g='image',x4='both',x5='horizontal',x9='rgba(173,173,173,1.00)';var g7='chef.png',g8='chef.mp4';var im='images/',aud='media/',vid='',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:x4,cg:x5,rI:n,cn:{dom:[{id:'chef',t:g,r:['459px','49px','107px','107px','auto','auto'],f:[x6,im+g7,'0px','0px']},{id:'chef2',t:'video',tag:'video',r:['120px','225px','786px','442px','auto','auto'],cn:'controls',sr:[vid+g8],pr:'auto'}],style:{'${Stage}':{isStage:true,r:['null','null','1024px','768px','auto','auto'],overflow:'hidden',f:[x9]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-98278785");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${chef}","click",function(sym,e){window.open("chef.html","_self");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-98278785");
